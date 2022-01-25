8c58e6900b9f1ffe6a34b482d363dc46d818942d
Ayush Agarwal <agarwalayush161@gmail.com>
2022-01-24 17:59:25 -0800

    added starter code

---
+++ reader.c
@@ -0,0 +1,24 @@
+/*
+ * CSE30 WI22 HW3
+ * Username: cse30wi22xx (Fill in your username)
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    /* Local Variables */
+    
+    /* insert your getopt loop here... */
+
+    /* save the other cli args */
+
+    /* process the input as described in the writeup */
+
+    /* Format string for output to be printed when -c option is given
+       
+       printf("Number of lines: %d\n "
+       "Longest field: %d characters\n", 
+       insert your variable names here); 
+    */
+}
